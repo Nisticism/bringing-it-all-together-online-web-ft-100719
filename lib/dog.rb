7f8791ac9a05bdc919c278fe92d22750e5b9b3cf
Hans Foster <fosterhans@gmail.com>
2019-12-10 00:21:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,8 +29,8 @@ class Dog
     DB[:conn].execute(sql)
   end
   
-  def self.create(name:, breed:)
-    dog = Dog.new(name, breed)
+  def self.create(id: nil, name:, breed:)
+    dog = Dog.new(id: id, name: name, breed: breed)
     dog.save
     dog
   end
